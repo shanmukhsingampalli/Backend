cd4b181a92f03702b6b2a507cc67b8334bcf818e
shanmukhsingampalli <shanmukhsingampalli9@gmail.com>
2026-03-07 19:16:00 +0530

    feat: implement API error handling and response classes; configure CORS and cookie parsing; update environment variables and dependencies

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({ path: "./env" });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MOngoDB connection failed", err);
+  });
 
 /*
 import express from "express";
